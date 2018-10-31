c9c225c7ab91c009822c253c44b2b1ea35664e2e
Zeu Fung <zomcoo@gmail.com>
2018-10-31 22:32:25 +0800

    bugs fixed

--- scripts/speaker.js
+++ scripts/speaker.js
@@ -40,6 +40,27 @@ if (!token) {
 EXCLM_WARN = true;
 EXCLM_REGEX = /[\u0021\u01C3\u203C\u2048\u2049\u26A0\u2755\u2757\u2762\u2763\uA71D\uA71E\uA71F\uFE57\uFF01]/gu;
 BOT_NAME = '传声筒';
+AUTHOR = 'zomcoo@gmail.com';
+
+// 仿照SDK, 构造回复消息的结构体
+const packEnvelope = (channelId, uid, key) => {
+  if (!channelId || !uid || !key) {
+    return null;
+  }
+  return {
+    room: { vchannel_id: channelId },
+    user: {
+      message: {
+        channel_id: channelId,
+        key,
+        type: 'channel_message',
+        uid,
+        vchannel_id: channelId,
+      },
+    },
+  };
+};
+
 
 module.exports = async (robot) => {
 
@@ -51,8 +72,9 @@ module.exports = async (robot) => {
     if (message.type !== 'channel_message') {
       return;
     }
-    // 叹号警告
+
     if (message.subtype === 'normal') {
+      // 检查普通消息
       const match = message.text.match(EXCLM_REGEX);
       if (EXCLM_WARN && match) {
         const warning = warnings[message.uid] || [];
@@ -63,6 +85,7 @@ module.exports = async (robot) => {
         res.reply('滥用感叹号不是成熟的表现，我劝你冷静。');
       }
     } else if (message.subtype === 'forwarded') {
+      // 检查转发消息
       const { repost } = message;
       const match = repost.text.match(EXCLM_REGEX);
       if (EXCLM_WARN && match) {
@@ -71,22 +94,13 @@ module.exports = async (robot) => {
           length: match.length,
           total: message.text.length,
         });
-        const newEnvelope = {
-          room: { vchannel_id: message.vchannel_id },
-          user: {
-            message: {
-              channel_id: message.vchannel_id,
-              key: message.key,
-              type: 'channel_message',
-              uid: message.uid,
-              vchannel_id: message.vchannel_id,
-            },
-          },
-        };
+        const newEnvelope = packEnvelope(message.vchannel_id, message.uid, message.key);
         const newText = '你在退群的边缘试探，答应我别转发滥用感叹号的消息好吗？';
         const newMessage = robot.adapter.client.packMessage(true, newEnvelope, [newText]);
         robot.adapter.client.sendMessage(newEnvelope, newMessage);
       }
+    } else {
+      // 其他消息不做处理
     }
   });
 
@@ -98,70 +112,74 @@ module.exports = async (robot) => {
     if (message.type !== 'message') {
       return;
     }
-    // 根据从讨论组的转发消息, 仿照SDK的reply, 构造新消息结构体
+
     if (message.subtype === 'forwarded') {
+      // 转发文字消息
       const { repost } = message;
-      const envelope = {
-        room: { vchannel_id: repost.vchannel_id },
-        user: {
-          message: {
-            channel_id: repost.vchannel_id,
-            key: repost.message_key,
-            type: 'channel_message',
-            uid: repost.uid,
-            vchannel_id: repost.vchannel_id,
-          },
-        },
-      };
-      res.send('对以上转发消息有何看法。');
+      const envelope = packEnvelope(repost.vchannel_id, repost.uid, repost.message_key);
+      res.send('我是你的传声筒，有话请说。');
       envelopes[message.uid] = envelope;
-    }
-    // 根据接收消息，替换消息中的叹号为空格，发送新消息到特定讨论组
-    const newEnvelope = envelopes[message.uid];
-    if (message.subtype === 'normal' && newEnvelope) {
-      const channelInfo = await bearychat.channel
-        .info({ token, channel_id: newEnvelope.room.vchannel_id })
-        .then(resp => resp.json())
-        .catch(err => robot.logger.error(err));
-      const { is_member } = channelInfo;
-      if (is_member === true) {
-        let newText = EXCLM_WARN ? message.text.replace(EXCLM_REGEX, ' ') : message.text;
-        const newMessage = robot.adapter.client.packMessage(true, newEnvelope, [newText]);
-        robot.adapter.client.sendMessage(newEnvelope, newMessage);
-      } else if (is_member === false) {
-        res.send(`@${BOT_NAME} 还不是讨论组 #${channelInfo.name} 的成员。`);
+    } else if (message.subtype === 'share_file') {
+      // 转发文件消息, 因为转发消息里没附带原始信息的key，所以暂不支持
+      // const { file } = message;
+      // const envelope = packEnvelope(file.channel_id, file.uid, file.key);
+      // res.send('我是你的传声筒，有话请说。');
+      // envelopes[message.uid] = envelope;
+      res.send('暂不支持回复文件类型消息。');
+    } else if (message.subtype === 'normal') {
+      // 处理普通消息
+      const newEnvelope = envelopes[message.uid];
+      if (newEnvelope) {
+        // 缓存了转发消息，则以对话消息为内容回复转发消息
+        const { is_member, name, error } = await bearychat.channel
+          .info({ token, channel_id: newEnvelope.room.vchannel_id })
+          .then(resp => resp.json())
+          .catch(err => robot.logger.error(err));
+        if (is_member === true) {
+          let newText = EXCLM_WARN ? message.text.replace(EXCLM_REGEX, ' ') : message.text;
+          const newMessage = robot.adapter.client.packMessage(true, newEnvelope, [newText]);
+          robot.adapter.client.sendMessage(newEnvelope, newMessage);
+        } else if (is_member === false) {
+          res.send(`@${BOT_NAME} 还不是讨论组 #${name} 的成员。`);
+        } else if (error) {
+          res.send(`出了点小问题，请稍后重试。（${error || ''}）`);
+        } else {
+          res.send(`出大问题了, 请联系作者。（${AUTHOR}）`);
+        }
+        envelopes[message.uid] = null;
       } else {
-        const { error = '' } = channelInfo;
-        res.send(`出了点小问题，请稍后重试。（${error}）`);
+        // 没有缓存转发消息，则回复教程
+        robot.emit('bearychat.attachment', {
+          message: res.message,
+          text: '使用方法',
+          attachments: [
+            {
+              text: '把想要匿名回复的消息转发给我，顺便说点什么，我就是你的传声筒',
+            },
+            {
+              images: [
+                { 
+                  url: 'https://github.com/zomco/hubot-speaker/blob/master/resources/example2.gif?raw=true',
+                }
+              ]
+            }
+          ]
+        });
       }
-      envelopes[message.uid] = null;
-    } else if (message.subtype === 'normal' && !newEnvelope) {
-      robot.emit('bearychat.attachment', {
-        message: res.message,
-        text: '使用方法',
-        attachments: [
-          {
-            text: '把想要匿名回复的消息转发给我，顺便说点什么，我就是你的传声筒',
-          },
-          {
-            images: [
-              { 
-                url: 'https://github.com/zomco/hubot-speaker/blob/master/resources/example.gif?raw=true',
-              }
-            ]
-          }
-        ]
-      });
+    } else {
+      // 未知消息类型
+      res.send(`${BOT_NAME} 无法理解你的话。`);
     }
 
   });
 
   // 错误处理
   robot.error((err, res) => {
-    robot.logger.error('DOES NOT COMPUTE');
+    robot.logger.error(err);
     if (res) {
-      res.reply('出了点小问题，请稍后重试。');
+      res.reply(`出大问题了, 请联系作者。（${AUTHOR}）`);
     }
+    process.exit(1);
   });
 
 };
